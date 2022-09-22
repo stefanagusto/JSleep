1906489efa618c0fd15c6dd7d40a8a42b4875002
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2022-09-22 20:41:21 +0700

    Post Test Modul 2

--- JSleep/StefanAgustoHutapeaJSleepDN/Account.java
+++ JSleep/StefanAgustoHutapeaJSleepDN/Account.java
@@ -5,14 +5,5 @@ public class Account
     // instance variables 
     public String name;
     public String email;
-    public String password;
-
-    //constructor Account()
-    public Account()
-    {
-        // initialise instance variables
-        this.name = "";
-        this.email = "";
-        this.password = "";
-    }
+    public String password;  
 }
