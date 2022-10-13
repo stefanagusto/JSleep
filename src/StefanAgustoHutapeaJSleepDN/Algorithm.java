754e0bb64c2355fd2e9a63698d19edbc87196ce2
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2022-10-13 22:32:24 +0700

    Post Test 5

--- src/StefanAgustoHutapeaJSleepDN/Algorithm.java
+++ src/StefanAgustoHutapeaJSleepDN/Algorithm.java
@@ -1,8 +1,53 @@
 package StefanAgustoHutapeaJSleepDN;
 import java.util.Iterator;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Algorithm {
+    public static <T> List<T> collect(Iterable<T> iterable, T value) {
+        List<T> list = new ArrayList<T>();
+        Iterator<T> iterator = iterable.iterator();
+        while (iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+        list.add(value);
+        return list;
+    }
+
+    public static <T> List<T> collect(Iterable<T> iterable, Predicate<T>pred) {
+        List<T> list = new ArrayList<T>();
+        Iterator<T> iterator = iterable.iterator();
+        while (iterator.hasNext()) {
+            T value = iterator.next();
+            if (pred.predicate(value)) {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> collect(T[]array, Predicate<T>pred) {
+        List<T> list = new ArrayList<T>();
+        for (T value : array) {
+            if (pred.predicate(value)) {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> collect(Iterator<T>iterator, Predicate<T>pred) {
+        List<T> list = new ArrayList<T>();
+        while (iterator.hasNext()) {
+            T value = iterator.next();
+            if (pred.predicate(value)) {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
     public static <T> int count(Iterator<T>iterator, T value)
     {
         final Predicate<T> pred = value::equals;
@@ -106,5 +151,45 @@ public class Algorithm {
                 return true;
         }
         return false;
+    }
+
+    public static <T> List<T> paginate(T[] array, int page, int pageSize, Predicate<T>pred) {
+        List<T> list = new ArrayList<T>();
+        int start = page * pageSize;
+        int end = start + pageSize - 1;
+        for (int i = start; i < end; i++) {
+            T value = array[i];
+            if (pred.predicate(value)) {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> paginate(Iterator<T> iterator, int page, int pageSize, Predicate<T>pred) {
+        List<T> list = new ArrayList<T>();
+        int start = page * pageSize;
+        int end = start + pageSize - 1;
+        for (int i = start; i < end; i++) {
+            T value = iterator.next();
+            if (pred.predicate(value)) {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> paginate(Iterable<T> iterable, int page, int pageSize, Predicate<T>pred) {
+        List<T> list = new ArrayList<T>();
+        int start = page * pageSize;
+        int end = start + pageSize - 1;
+        Iterator<T> iterator = iterable.iterator();
+        for (int i = 0; i < end; i++) {
+            T value = iterator.next();
+            if (i >= start && pred.predicate(value)) {
+                list.add(value);
+            }
+        }
+        return list;
     }
 }
