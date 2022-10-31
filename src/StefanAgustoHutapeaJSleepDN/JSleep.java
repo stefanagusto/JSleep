65ff15c1e78158a6b3a05daf16a3c8d358aa7b80
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2022-10-31 21:14:23 +0700

    Tugas Pendahuluan 6

--- src/StefanAgustoHutapeaJSleepDN/JSleep.java
+++ src/StefanAgustoHutapeaJSleepDN/JSleep.java
@@ -1,25 +1,39 @@
 package StefanAgustoHutapeaJSleepDN;
-import java.sql.Date;
-import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 /**
- * Case Study Modul 5
+ * Tugas Pendahuluan 6
  * Stefan Agusto Hutapea
  * 2106700744
  */
 
 public class JSleep
 {   
-    public static Room createRoom(){
-        return new Room("Room 1", 2, new Price(1, 100000), Facility.AC, City.JAKARTA, "Jl. Jalan");
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
     }
 
     public static void main(String[] args)
     {
-        ArrayList<Room> RoomSerialized = new ArrayList<Room>();
-        for(int i = 0; i<5; i++){
-            RoomSerialized.add(i, JSleep.createRoom());
-            System.out.println(RoomSerialized.get(i).toString());
+        String filepath= "C:\\Users\\ACER NITRO 5\\OneDrive - UNIVERSITAS INDONESIA\\DTE\\Mata Kuliah\\Sem 3\\Pemrograman Berorientasi Objek & Praktikum\\JSleep\\JSleep\\city.json";
+        Gson gson = new Gson();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("Name: " + input.name);
+            System.out.println("Population: " + input.population);
+            System.out.println("States: ");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch(IOException e){
+            e.printStackTrace();
         }
     }
 }
